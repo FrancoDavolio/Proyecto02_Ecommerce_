17a4f3d246cb646ab45aa58b9e3fc0d57f8063d7
GabrielPerlo <gabrielperloestelastalitas@gmail.com>
2022-09-09 17:07:04 -0300

    resolviendo problema de codigo

--- js/app.js
+++ js/app.js
@@ -27,3 +27,28 @@ window.addEventListener("load", function () {
       ]
   });
 });
+
+
+// verrificar el localStorage
+let listaProducto = JSON.parse(localStorage.getItem("listaProductoKey")) || [];
+let padre = document.querySelector("#grillaProducto");
+
+if (listaProducto.length > 0) {
+  listaProducto.map((producto) => {
+    crearColumna(producto);
+  });
+} else {
+  padre.innerHTML = '<h2 class="text-center">No hay productos cargadas</h2>';
+}
+
+function crearColumna(producto){
+  console.log(producto);
+  padre.innerHTML += ` <div class="card col-md-3 m-3" style="width: 18rem;">
+  <img src="${producto.modificarImagen}" class="card-img-top" alt="monitor">
+  <div class="card-body">
+      <h5 class="card-title">${producto.modificarNombre}</h5>
+      <p class="card-text">Precio:${producto.modificarPrecio} </p>
+      <button class="btn btn-primary" type="button" onclick="verDetalle(´${producto.modificarCodigo}´)">Ver mas</button>
+  </div>
+</div>`;
+}
